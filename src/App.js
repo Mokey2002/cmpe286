56125dda3ee4e5f2ca290efbc8b1501661dc0db7
Ubuntu <ubuntu@ip-172-31-77-186.ec2.internal>
2021-05-07 04:55:15 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import S3FileUpload from 'react-s3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const config ={
+  bucketName: "cmpe286",
+  region:"us-east-1",
+  accessKeyId:"AKIAVFQCFDNP2J7OEYOA",
+  secretAccessKey:"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",
+}
+
+class App extends Component {
+  upload=(event)=>{
+    S3FileUpload.uploadFile(event.target.files[0], config)
+    .then((data)=> {
+      console.log(data.location)
+    })
+    .catch((err)=>{
+      alert(err)
+    })
+  }
+  render(){
+    return (
+      <div>
+        <h3>Upload An Image</h3>
+        <input type="file" onChange={this.upload}/>
+      </div>
+    );
+  }
 }
 
 export default App;
